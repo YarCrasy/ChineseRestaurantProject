9707dd27414dbc2b983bf386546a7864a371ca76
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2025-02-15 12:54:35 +0000

    Sections

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -5,8 +5,24 @@ function HomePage(prop) {
     return (
         <main>
             <section className="best-seller-section" id="section-0">
+                <h2>Our Best Sellers</h2>
+                <div className="best-seller content">
+                    {prop.dishes.map((dish, index) => (
+                        <SimpleDishCard dish={dish} key={index} />
+                    ))}
+                </div>
+            </section>
+            <section className="best-seller-section" id="section-1">
                 <h2>Welcome to the Main Page</h2>
-                <div className="best-seller-content">
+                <div className="best-seller content">
+                    {prop.dishes.map((dish, index) => (
+                        <SimpleDishCard dish={dish} key={index} />
+                    ))}
+                </div>
+            </section>
+            <section className="best-seller-section" id="section-2">
+                <h2>Welcome to the Main Page</h2>
+                <div className="best-seller content">
                     {prop.dishes.map((dish, index) => (
                         <SimpleDishCard dish={dish} key={index} />
                     ))}
