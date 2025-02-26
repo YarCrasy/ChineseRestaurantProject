3988397515a57a992d1186925565f366ac3e7cdd
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2025-02-26 18:55:10 +0000

    MinorChanges

--- src/components/dish-card/DishCard.jsx
+++ src/components/dish-card/DishCard.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import "./DishCard.css";
 
 function DishCard(props) {
+    const [showPanel, setShowPanel] = useState(false);
     let dish = props.dish;
+
+    const handleButtonClick = () => {
+        setShowPanel(true);
+    };
+
     return (
         <div className="dish-card">
             <div className="dish-id">
@@ -9,8 +16,16 @@ function DishCard(props) {
             </div>
             <img src={dish.image} alt={dish.name} />
             <h3>{dish.name}</h3>
-            <p>{dish.description}</p>
-            <p>{dish.price}</p>
+            <p className="dish-description">{dish.description}</p>
+            <div className="bottom-container">
+                <p className="price-tag">{dish.price}€</p>
+                <button className="add-to-cart" onClick={handleButtonClick}>Buy</button>
+            </div>
+            {showPanel && (
+                <div className="wip-panel">
+                    <p>Work in progress</p>
+                </div>
+            )}
         </div>
     );
 }
