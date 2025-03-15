4670b384e99456bdacadb76a9b09364a89e454a1
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2025-03-15 02:11:09 +0000

    addingUserIcon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,29 +14,39 @@ function Header() {
     return (
         <header>
             <nav className="nav">
+
                 <a href="/home">
                     <div className="logo-container">
                         <img src={logo} alt="Logo" className="logo-img" />
                         <h1 className="logo">Chuan Min Fusion</h1>
                     </div>
                 </a>
+
                 <button className="menu-toggle">
                     <img src={menuIcon} alt="Menu" className="menu-icon" />
                 </button>
+
                 <div className="nav-menu">
                     <div className="nav-links">
                         <a href="/menu">Menu</a>
                         <a href="/contact">Contact</a>
-                        {/* to implement */}
-                        {/* <div className="lang-dropdown">
+                        <div className="lang-dropdown">
                             <button className="dropdown-btn">EN ▼</button>
                             <div className="dropdown-content">
                                 <a data-lang="en">English</a>
                                 <a data-lang="es">Español</a>
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                 </div>
+
+                <button className="user-profile">
+                    <div className="logo-container">
+                        <img src={logo} alt="Logo" className="logo-img" />
+                    </div>
+                </button>
+
+
             </nav>
         </header>
     );
